eb03fe539c3571fdf1a1ea6810f1d39bb8c82897
Davide Restivo <davide.restivo@swisscom.com>
2017-10-20 18:07:58 +0200

    Add exception handling in saveconfig.py

--- telnetlib/saveconfigs.py
+++ telnetlib/saveconfigs.py
@@ -9,22 +9,25 @@ hosts = ("192.168.0.1",)
 def get_config(host, username, password):
         print ("Getting running-config of: " + host)
         HOST = host.strip()
-        tn = telnetlib.Telnet(HOST)
-        tn.read_until(b"Username: ")
-        tn.write(username.encode('ascii') + b"\n")
-        if password:
-            tn.read_until(b"Password: ")
-            tn.write(password.encode('ascii') + b"\n")
+        try:
+                tn = telnetlib.Telnet(HOST)
+                tn.read_until(b"Username: ")
+                tn.write(username.encode('ascii') + b"\n")
+                if password:
+                        tn.read_until(b"Password: ")
+                        tn.write(password.encode('ascii') + b"\n")
 
-        tn.write(b"terminal length 0\n")
-        tn.write(b"show run\n")
-        tn.write(b"exit\n")
+                tn.write(b"terminal length 0\n")
+                tn.write(b"show run\n")
+                tn.write(b"exit\n")
 
-        readoutput = tn.read_all().decode('ascii')
-        saveoutput = open("switch-" + HOST, "w+")
-        saveoutput.write(readoutput)
-        saveoutput.write("\n")
-        saveoutput.close
+                readoutput = tn.read_all().decode('ascii')
+                saveoutput = open("switch-" + HOST, "w+")
+                saveoutput.write(readoutput)
+                saveoutput.write("\n")
+                saveoutput.close
+        except OSError as err:
+                print("OS error: {0}".format(err))
 
 if __name__ == "__main__":
         # Get Username and Password
